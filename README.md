944a1fe335ab4b20a7ba1a40ab05f9747864dfd3
Thiago Amaral <it.eng.amaraldepaula@gmail.com>
2021-02-10 07:18:52 -0300

    fix logic - working with txt file as argument

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
-# Not working yet! I'm debugging the logic.
-# Subsort
-Subdomain Redundancies Sorter.
+## Subsort - The Subdomain Redundancies Sorter.
+This program removes subdomain redundancies and returns an optimized list. It saves A LOT of time when parsing large subdomain/dns files with grep.
+
+## Why?
+Because it can save you A LOT of time in some situations.
+
+Suppose you have a 30 Gigabytes .json file full of DNS entries. If you grep for the lines containing the domain "example.com", next you don't need to parse for "sub.example.com" as the results of the latter will be contained in the results of the former.
+
+## So what does it do, exactly?
+You provide a .txt file containing  domains and subdomains and this program removes the subdomain redundancies, returning an optimized list, which can later be used by another tool such as grep.
+
+So if you provide a .txt file as argument containing:
+'''
+example.com
+sub.example.com
+another.example.com
+test.com
+sub.test.com
+anothertest.com
+'''
+Then, the application would return:
+'''
+example.com
+test.com
+anothertest.com
+'''
+
+## How to Run
+python subsort.py subdomains_file.txt
+
+## Requirements
+Python 2.7 or higher
--- subsort.py
+++ subsort.py
@@ -9,35 +9,40 @@ if you grep for the lines containing the domain "example.com"
 you don't need to parse for "sub.example.com" as the results 
 of the latter will be contained in the results of the former.
 This program removes the subdomain redundancies, returning an
-optimized list, which can later be used at grep.
+optimized list, which can later be used at a parsing cmd tool
+such as grep.
 '''
 
 import argparse
 
 parser = argparse.ArgumentParser(description='Remove redundancies returning an optimized list')
-parser.add_argument('domains_txt_path', type=str, help='Path to the .txt file containing the list to be optimized')
-
+parser.add_argument('subdomains_txt_path', type=str, help='Path to the .txt file containing the list to be optimized')
 args = parser.parse_args()
 
-def remove_list_redundancies(domains_txt_path):
-    with open(domains_txt_path, 'r') as lfp:
+def del_file_subds_redund(subdomains_txt_path):
+    with open(subdomains_txt_path, 'r') as lfp:
         inscope_list = []
         for line in lfp:
-            inscope_list.append(line.strip('\n'))
+            # add '.' at the beginning of each subdomain to
+            # appropriately test subdomains occurrences in logic parsing.
+            inscope_list.append('.' + line.strip('\n'))
         
         sorted_inscope_list = sorted(inscope_list, key=len)
         inscope_range = len(sorted_inscope_list)
 
-        optimized_list = []
+        # main list to add unique subdomains.
+        optimized_list = [] 
 
         for i in range(inscope_range):
-                
-            disposable_list = []
-            disposable_list.extend(sorted_inscope_list)
-            disposable_list.remove(sorted_inscope_list[i])
-            disposable_list_string = str(disposable_list)
+            
+            # temporary list
+            temp_list = []
+            temp_list.extend(sorted_inscope_list)
+            temp_list.remove(sorted_inscope_list[i])
+            temp_list_string = str(temp_list)
 
-            if sorted_inscope_list[i] in disposable_list_string:
+            # main logic is on if-else
+            if sorted_inscope_list[i] in temp_list_string:
                 flag = 0
                 for item in optimized_list:
                     if item in sorted_inscope_list[i]:
@@ -45,14 +50,22 @@ def remove_list_redundancies(domains_txt_path):
                 if flag == 0:
                     optimized_list.append(sorted_inscope_list[i])
             else:
-                disposable_list_string_2 = str(optimized_list)
-                if sorted_inscope_list[i] not in disposable_list_string_2:
+                temp_list_string_2 = str(optimized_list)
+                if sorted_inscope_list[i] not in temp_list_string_2:
                     flag = 0
                     for item in optimized_list:
                         if item in sorted_inscope_list[i]:
                             flag += 1
                     if flag == 0:
                         optimized_list.append(sorted_inscope_list[i])
+
+    # removing '.' at the beginning of each subdomain (not necessary anymore).
+    for i, element in enumerate(optimized_list):
+        optimized_list[i] = element.strip('.')
+
+    # putting elements in alphabetical order.
+    optimized_list.sort() 
+
     return optimized_list
 
 def print_list_elements(list):
@@ -61,7 +74,8 @@ def print_list_elements(list):
 
 
 if __name__ == '__main__':
+
     optimal_subd_list = []
-    optimal_subd_list.extend(remove_list_redundancies(args.domains_txt_path))
+    optimal_subd_list.extend(del_file_subds_redund(args.subdomains_txt_path))
 
     print_list_elements(optimal_subd_list)
